7b6086e565a64f08b3f64050431044627550d9c6
John Bodenschatz <bodensjc@mail.uc.edu>
2021-01-25 23:09:28 -0500

    alex help

--- temp.py
+++ temp.py
@@ -1,24 +1,28 @@
-def ebay_fee(sell_price):
-    """Returns the fees charged by ebay.com given the selling
-    price of fixed-price books, movies, music, or video games.
-    fee is $0.50 to list plus 13% of selling price up to $50.00,
-    5% of amount from $50.01 to $1000.00, and 
-    2% for amount $1000.01 or more."""
+import shutil, os
+from os import walk
 
-    p50 = 0.13  # for amount $50 and lower
-    p50_to_1000 = 0.05  # for $50.01-$1000
-    p1000 = 0.02  # for $1000.01 and higher
-    fee = 0.50  # fee to list item
+myfilenames = ['test1.txt', 'test3.txt'] #replace with
+parent = 'C:\\Users\\johnb\\Documents\\python\\testRepo' #replace with your parent directory that holds all of the P0 files
+os.chdir(parent)
+_, pfolders, _ = next(walk(parent))
 
-    if sell_price <= 50:
-        fee  = fee + (sell_price*p50)
-    elif sell_price <= 1000:
-        fee = fee + (50*p50) + ((sell_price-50)*p50_to_1000)
-    else:
-        fee = fee + (50*p50) + ((1000-50)*p50_to_1000) \
-                  + ((sell_price-1000)*p1000)
 
-    return fee
+def pcopy(source, dest):
+    _, _, filenames = next(walk(source))
+    for f in filenames:
+        if f in myfilenames:
+            shutil.copy(f, dest)
 
-selling_price = float(input('Enter item selling price (ex: 65.00): '))
-print('Ebay fee: $', ebay_fee(selling_price))
+
+for pfolder in pfolders:
+    currentPath = os.path.join(parent, pfolder)
+    destPath = os.path.join(parent, pfolder+'copy')
+    _, first, _ = next(walk(currentPath))
+    os.mkdir(destPath)
+    pcopy(currentPath, destPath)
+
+
+
+
+
+
--- testPull.py
+++
@@ -1,4 +0,0 @@
-import pandas
-import numpy
-
-#this is a test import file, created on github to be imported locally
